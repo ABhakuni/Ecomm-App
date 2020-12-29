9a204638c2c0c9539864586e71e82a83b8658df4
anurag28 <anurag@janitri.in>
2020-12-30 04:31:34 +0530

    login and product page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import LoginLayout from './components/login/loginlayout.jsx';
 import './App.css';
+import Products from './components/products/products.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-           Welcome to Playground!
-          </p>
+          {/* <LoginLayout /> */}
+          <Products />
         </header>
       </div>
     );
